88ff698869218bd00e1e8c968e2ec3a3dae5ff16
Evan Kline Mores <113956467+moresevankline@users.noreply.github.com>
2022-11-07 15:32:43 -0800

    Add files via upload

---
+++ Restaurant.py
@@ -0,0 +1,11 @@
+class Restaurant():
+    
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        
+    def describe_restaurant(self):
+
+    def open_restaurant():
+        print("Welcome the restaurant is open!!!")
+
