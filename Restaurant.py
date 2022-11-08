1c290102d14faa862f53185603e134de0ebd5e77
Evan Kline Mores <113956467+moresevankline@users.noreply.github.com>
2022-11-07 17:13:43 -0800

    Update Restaurant.py

--- Restaurant.py
+++ Restaurant.py
@@ -13,9 +13,28 @@ class Restaurant():
         print("Welcome to " + self.restaurant_name)
         print("We serve the best " + self.signature_dish_name)
 
+class Users():
+    
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def describe_user(self):
+        print("Hello " + self.first_name + " " + self.last_name)
+        print("What's on your order?")
+
 restaurant = Restaurant("Jollibee", "Fried Chicken")
 
 restaurant.describe_restaurant()
 print("\n")
 restaurant.open_restaurant()
+print("\n")
+
+fname = input(str("What's your first name? "))
+lname = input(str("How about your last name? "))
+print("\n")
+
+user = Users(fname, lname)
+
+user.describe_user()
 
