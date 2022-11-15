0fc01b610605dc75c3f23514b3bfe0ad3606f130
Evan Kline Mores <113956467+moresevankline@users.noreply.github.com>
2022-11-14 16:00:57 -0800

    Update Restaurant.py

--- Restaurant.py
+++ Restaurant.py
@@ -1,40 +1,46 @@
 class Restaurant():
-    
-    def __init__(self, restaurant_name, signature_dish_name):
+
+    def __init__(self, restaurant_name, cuisine_type, number_served):
         self.restaurant_name = restaurant_name
-        self.signature_dish_name = signature_dish_name
-        
+        self.cuisine_type = cuisine_type
+        self.number_served = 0
+    
     def describe_restaurant(self):
-        print("Restaurant Name : " + self.restaurant_name)
-        print("Signature Dish : " + self.signature_dish_name)
+        print("Restaurant Name: " + self.restaurant_name)
+        print("Cuisine Type: " + self.cuisine_type)
+        print("Number Served: " + str(self.number_served))
 
     def open_restaurant(self):
-        print(self.restaurant_name + " is Now Open")
-        print("Welcome to " + self.restaurant_name)
-        print("We serve the best " + self.signature_dish_name)
+        print("Restaurant is open!\n")
 
-class Users():
-    
-    def __init__(self, first_name, last_name):
+class User():
+    def __init__(self, first_name, middle_initial, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
+        self.middle_initial = middle_initial
+        self.age = age
 
     def describe_user(self):
-        print("Hello " + self.first_name + " " + self.last_name)
-        print("What's on your order?")
+        print("First Name: " + self.first_name)
+        print("Middle Initial: " + self.middle_initial)
+        print("Last Name: " + self.last_name)
+        print("Age: " + str(self.age))
 
-restaurant = Restaurant("Jollibee", "Fried Chicken")
+    def greet_user(self):
+        print("\nHello " + self.first_name + " Welcome!!!")
 
-restaurant.describe_restaurant()
-print("\n")
-restaurant.open_restaurant()
-print("\n")
+restaurant1 = Restaurant("Max", "Main Dish", 20)
+restaurant1.describe_restaurant()
+restaurant1.open_restaurant()
 
-fname = input(str("What's your first name? "))
-lname = input(str("How about your last name? "))
-print("\n")
+restaurant2 = Restaurant("McDonalds", "Fast Food", 19)
+restaurant2.describe_restaurant()
+restaurant2.open_restaurant()
 
-user = Users(fname, lname)
+restaurant3 = Restaurant("Jollibee", "Fast Food", 0)
+restaurant3.describe_restaurant()
+restaurant3.open_restaurant()
 
-user.describe_user()
-
+user1 = User("Sherlock", "J.", "Holmes", 29)
+user1.describe_user()
+user1.greet_user()
